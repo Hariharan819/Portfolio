ee42a6da0d50dc984d9980bc50efabcd8d390280
Hariharan <hariharanvenkatachalam11@gmail.com>
2024-12-06 14:53:00 +0530

    resume

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,6 @@
 import logo from "./images/logo.png";
 import bannerImg from "./images/bannerImg.png";
-import projectOne from "./images/projects/projectOne.webp";
+import projectOne from "./images/projects/projectOne.png";
 import projectTwo from "./images/projects/projectTwo.png";
 import projectThree from "./images/projects/projectThree.png";
 import contactImg from "./images/contact/contactImg.png";
